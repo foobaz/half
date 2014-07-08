f80a363fba2344e2d35871e534fe1817ecf63f6b
William MacKay <foobaz@gmail.com>
2014-07-07 22:22:04 -0700

    utility functions for encoding and decoding from slices, fleshed out readme

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 half
 ====
 
-Half-precision 16-bit floating point
+This package provides IEEE 754 compliant functions for converting to and
+from half-precision, 16-bit, floating point numbers. It properly handles
+NaNs, infinities, and subnormal numbers.
+
+###Purpose
+
+This package is primarily intended for use with OpenGL. By specifying vertexes
+or textures as 16-bit floats, you can save video memory, improve buffer
+upload times, and draw faster. Besides OpenGL, 16-bit floats are also used in
+many image formats, such as OpenEXR, for high dynamic range and deep color.
+
+###Limitations
+The range and precision of a 16-bit float are much less than a 32-bit float.
+They can represent positive and negative values as small as 0.000070 and as
+large as 65472.0 with approximately 3 decimal digits of precision. For
+example, pi is rounded to 3.140625.
+
+###Credit
+The conversion functions are based on a Stack Overflow post by Paul
+"Phernost" Tessier:
+http://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
--- half.go
+++ half.go
@@ -1,8 +1,16 @@
 package half
 
-import "math"
+import (
+	"encoding/binary"
+	"math"
+)
 
 type Float16 uint16
+
+// This code is adapted from Paul "Phernost" Tessier's answer on Stack Overflow:
+// http://stackoverflow.com/questions/1659440/32-bit-to-16-bit-floating-point-conversion
+//
+// Ported from C to Go and generalized to 64-bit by William MacKay
 
 const mantissaLength16 = 10
 const mantissaLength32 = 23
@@ -14,14 +22,14 @@ const shift64 = mantissaLength64 - mantissaLength16
 const shiftSign32 = 32 - 16
 const shiftSign64 = 64 - 16
 
-const infN32 = 0x7F800000 // flt32 infinity
+const infN32 = 0x7F800000         // flt32 infinity
 const infN64 = 0x7FF0000000000000 // flt64 infinity
-const maxN32 = 0x477FE000 // max flt16 normal as a flt32
+const maxN32 = 0x477FE000         // max flt16 normal as a flt32
 const maxN64 = 0x40EFFC0000000000 // max flt16 normal as a flt64
-const minN32 = 0x38800000 // min flt16 normal as a flt32
+const minN32 = 0x38800000         // min flt16 normal as a flt32
 const minN64 = 0x3F10000000000000 // min flt16 normal as a flt64
-const signN32 = 1 << (32 - 1) // flt32 sign bit
-const signN64 = 1 << (64 - 1) // flt64 sign bit
+const signN32 = 1 << (32 - 1)     // flt32 sign bit
+const signN64 = 1 << (64 - 1)     // flt64 sign bit
 
 const infC32 = infN32 >> shift32
 const infC64 = infN64 >> shift64
@@ -33,13 +41,13 @@ const minC32 = minN32 >> shift32
 const minC64 = minN64 >> shift64
 const signC = 1 << (16 - 1) // flt16 sign bit
 
-const mulN32 = 0x52000000 // (1 << mantissaLength32) / minN32
+const mulN32 = 0x52000000         // (1 << mantissaLength32) / minN32
 const mulN64 = 0x4410000000000000 // (1 << mantissaLength64) / minN64
-const mulC32 = 0x33800000 // minN32 / (1 << mantissaLength16)
+const mulC32 = 0x33800000         // minN32 / (1 << mantissaLength16)
 const mulC64 = 0x3E70000000000000 // minN64 / (1 << mantissaLength16)
 
 const subC = ^(-1 << mantissaLength16) // max flt16 subnormal down shifted
-const norC = 1 << mantissaLength16 // min flt16 normal down shifted
+const norC = 1 << mantissaLength16     // min flt16 normal down shifted
 
 const maxD32 = infC32 - maxC32 - 1
 const maxD64 = infC64 - maxC64 - 1
@@ -148,4 +156,34 @@ func (x Float16) To64() float64 {
 
 	v |= sign
 	return math.Float64frombits(v | sign)
+}
+
+func FromBigEndian(b []byte) Float16 {
+	x := binary.BigEndian.Uint16(b)
+	return Float16(x)
+}
+
+func FromLittleEndian(b []byte) Float16 {
+	x := binary.LittleEndian.Uint16(b)
+	return Float16(x)
+}
+
+func (x Float16) PutBigEndian(b []byte) {
+	binary.BigEndian.PutUint16(b, uint16(x))
+}
+
+func (x Float16) PutLittleEndian(b []byte) {
+	binary.LittleEndian.PutUint16(b, uint16(x))
+}
+
+func (x Float16) BigEndian() []byte {
+	b := make([]byte, 2)
+	x.PutBigEndian(b)
+	return b
+}
+
+func (x Float16) LittleEndian() []byte {
+	b := make([]byte, 2)
+	x.PutLittleEndian(b)
+	return b
 }
